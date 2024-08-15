4d1f23ee58d9ba1a6cf609d6f90bba4c75cf6d67
valya_plotnikova <valya_ezhova@mail.ru>
2024-08-15 11:29:23 +0300

    update forms

--- mailing/forms.py
+++ mailing/forms.py
@@ -1,10 +1,24 @@
 from django import forms
 
-from mailing.models import Client
+from mailing.models import Client, Message, Mail
 
 
 class ClientForm(forms.ModelForm):
 
     class Meta:
         model = Client
+        fields = "__all__"
+
+
+class MessageForm(forms.ModelForm):
+
+    class Meta:
+        model = Message
+        fields = "__all__"
+
+
+class MailForm(forms.ModelForm):
+
+    class Meta:
+        model = Mail
         fields = "__all__"
