04f6a839c7b8b381d25f0dfb862f3af926fdbb47
valya_plotnikova <valya_ezhova@mail.ru>
2024-09-05 15:52:28 +0300

    update forms mailing

--- mailing/forms.py
+++ mailing/forms.py
@@ -1,24 +1,25 @@
 from django import forms
 
 from mailing.models import Client, Message, Mail
+from users.models import User
 
 
 class ClientForm(forms.ModelForm):
 
     class Meta:
         model = Client
-        fields = "__all__"
+        exclude = ('owner',)
 
 
 class MessageForm(forms.ModelForm):
 
     class Meta:
         model = Message
-        fields = "__all__"
+        exclude = ('owner',)
 
 
 class MailForm(forms.ModelForm):
 
     class Meta:
         model = Mail
-        fields = "__all__"
+        exclude = ('is_active', 'owner')
