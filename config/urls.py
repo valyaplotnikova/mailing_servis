568e82b171b4a5ca89f1e68e0d25bdaedca90ffb
valya_plotnikova <valya_ezhova@mail.ru>
2024-09-06 15:44:44 +0300

    update config

--- config/urls.py
+++ config/urls.py
@@ -25,4 +25,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mailing.urls', namespace='mailing')),
     path('users/', include('users.urls', namespace='users')),
+    path('blog/', include('blog.urls', namespace='blog')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
